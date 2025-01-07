276865b868c3f17767b3dd75f13ffa89756ac9de
TumbleWeedBro <munashwengz@gmail.com>
2025-01-07 22:41:56 +0200

    change to section 3

--- docs/api-request.js
+++ docs/api-request.js
@@ -1,17 +1,16 @@
+const axios = require('axios');
 const fs = require('fs');
-let data;
+
 let topic = 'Lawsuit';
-let category = 'Business'
+let category = 'Business';
 let fromDate = '2024-01-01';
 let query = "lawsuit";
-const axios = require('axios');
-const apiKey ="18fadae1-3d0d-4524-b463-46b75325369f"
-const url = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&showReprints=false&paywall=false&sortBy=date&category=${category}&topic=${topic}&topic=Business Leaders&q=${query}`;
 
+const apiKey = "18fadae1-3d0d--b463-46b75325369f";
+const url = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&showReprints=false&paywall=false&sortBy=date&category=${category}&topic=${topic}&topic=Business Leaders&q=${query}`;
 
 axios.get(url)
   .then((response) => {
-    console.log('API Response:', response.data);  // Add this line to inspect the response
     const data = response.data;
     fs.writeFile('sponse.json', JSON.stringify(data, null, 2), (err) => {
       if (err) {
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public')); // Serve static files from the 'public' folder
+app.use(express.static('docs')); // Serve static files from the 'public' folder
 
 // Endpoint to read a file
 app.get('/read-file', (req, res) => {
