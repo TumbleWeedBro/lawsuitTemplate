383113a94d7ef04f5c3b4a42517ff7c7dffcc63a
TumbleWeedBro <munashwengz@gmail.com>
2025-01-08 11:34:25 +0200

    fixed an error in api-request

--- docs/api-request.js
+++ docs/api-request.js
@@ -1,33 +1,36 @@
-import {runQueryAndUpdateDOM} from './updateDom.js';
-
 const apiKey = "18fadae1-3d0d--b463-46b75325369f";
 
-const latestUpdatesUrl = `api.goperigon.com/v1/all?apiKey=[KEY]&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=date&category=Business&q=lawsuit`;
 
-const relevantResultsUrl = `api.goperigon.com/v1/all?apiKey=[KEY]&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=relevance&category=Business&q=lawsuit`; 
+import { runQueryAndUpdateDOM } from './updateDom.js';
 
-const businessUpdatesUrl= `https://api.goperigon.com/v1/all?apiKey=${apiKey}&showReprints=false&paywall=false&sortBy=date&category=business&topic=Business Leaders`;
+const latestUpdatesUrl = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=date&category=Business&q=lawsuit`;
 
-let searchQueryParam = "";
-const searchQueryUrl = `api.goperigon.com/v1/all?apiKey=[KEY]&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=relevance&q=${searchQueryParam}`;
+const relevantResultsUrl = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=relevance&category=Business&q=lawsuit`;
 
+const businessUpdatesUrl = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&showReprints=false&paywall=false&sortBy=date&category=business&topic=Business Leaders`;
+
+let searchQueryParam = '';
+const searchQueryUrl = `https://api.goperigon.com/v1/all?apiKey=${apiKey}&from=2025-01-06&sourceGroup=top100&showNumResults=true&showReprints=false&paywall=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=relevance&q=${searchQueryParam}`;
 
 // Fetch articles in order
 async function fetchArticlesInOrder() {
-  await getArticles(latestUpdatesUrl, "latestUpdates.json");
-  await getArticles(relevantResultsUrl, "relevantResults.json");
-  await getArticles(businessUpdatesUrl, "businessUpdates.json");
+  try {
+    await getArticles(latestUpdatesUrl, 'latestUpdates.json');
+    await getArticles(relevantResultsUrl, 'relevantResults.json');
+    await getArticles(businessUpdatesUrl, 'businessUpdates.json');
+  } catch (error) {
+    console.error('Error fetching articles:', error);
+  }
 }
 
 async function getArticles(url, fileName) {
-  await axios.get(url)
-    .then((response) => {
-      const data = response.data;
-      storeArticles(fileName, data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    storeArticles(fileName, data);
+  } catch (error) {
+    console.error('Error fetching from URL:', url, error);
+  }
 }
 
 function storeArticles(keyName, data) {
@@ -40,4 +43,5 @@ function storeArticles(keyName, data) {
 }
 
 fetchArticlesInOrder();
-runQueryAndUpdateDOM();
+
+// runQueryAndUpdateDOM();
